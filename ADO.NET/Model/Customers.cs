d2d24b70d15e9e9146ce3daa903c2a60af4a4196
irakli imesashvili <i.imesashvili@betsolutions.com>
2022-03-01 16:48:22 +0400

    Finished

---
+++ ADO.NET/Model/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.NET.Model
+{
+    public class Customers
+    {
+        //The class for customers
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
